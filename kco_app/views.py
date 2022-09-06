6ad05be7fd92ead3ed0a9f6f506c21075bec4255
khinchooo <khinchooo@mm.cybermissions.co.jp>
2022-09-06 15:04:54 +0900

    index追加

--- kco_app/views.py
+++ kco_app/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
+# from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-  return HttpResponse('<h1>Hello KhinChoOo App</h1>')
+  return render(request, 'kco_app/index.html')
