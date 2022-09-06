294ad6e89eea3e4236496bb6cc8a5e891c799ea5
HtunLinn <htunlinn@mm.cybermissions.co.jp>
2022-09-06 14:58:54 +0900

    index追加

--- hl_app/views.py
+++ hl_app/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-  return HttpResponse('<h1>Hello HL App</h1>')
+  return render(request, 'hl_app/index.html')
