b474b165df02e627fdbd65919910ed7bc959d1f9
K.Matsuo <k.matsuo@cybermissions.co.jp>
2022-09-06 14:58:28 +0900

    index.html 追加

--- matsuo_app/views.py
+++ matsuo_app/views.py
@@ -1,5 +1,4 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
-def index(requrest):
-    return HttpResponse('<h1>Hello Matsuo App</h1>')
+def index(request):
+    return render(request, 'matsuo_app/index.html')
