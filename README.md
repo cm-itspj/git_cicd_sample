661dbfc72adcf31a68833ecbc7fc88dd423241e4
K.Matsuo <k.matsuo@cybermissions.co.jp>
2022-09-06 13:40:20 +0900

    環境変数をGit管理しないように修正

--- README.md
+++ README.md
@@ -8,6 +8,7 @@ git clone https://github.com/cm-itspj/git_cicd_sample.git
 
 cd git_cicd_sample 
 ```
+
 2. Python 仮想環境設定
 ```
 python -m venv myvenv
@@ -15,7 +16,15 @@ source myvenv/bin/activate
 
 pip install -r requirements.txt
 ```
-3. サーバ起動
+
+3. .envファイル作成
+```
+touch .env
+echo "SECRET_KEY='xxxx'" >> .env 
+echo "DEBUG=True" >> .env
+```
+
+4. サーバ起動
 ```
 python manage.py migrate
 python manage.py runserver
--- mysite/settings.py
+++ mysite/settings.py
@@ -19,11 +19,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
+import environ
+env = environ.Env()
+env.read_env('.env')
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g-d5cj9%t9sqmk9rmpi!p(ef$*29=3z-lp)g+526jq%u_&f7xi'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = []
 
